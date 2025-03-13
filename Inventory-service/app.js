fe1b628637b436aafeb924590d51187330eaec9a
UvinduV <uvindusanketh470@gmail.com>
2025-03-13 23:19:38 +0530

    add inventory service

--- Inventory-service/app.js
+++ Inventory-service/app.js
@@ -3,7 +3,7 @@ const {eureka, Eureka} = require("eureka-js-client")
 
 //next js -- graph ql
 const app =express();
-const port =3000;
+const port =3002;
 
 const router =express.Router()
 
@@ -24,13 +24,32 @@ const eurekaClient =new Eureka({
             $: port,
             "@enabled":true
         },
-        dataCenterInfo
-    }
+        vipAddress: "inventory-service",
+        dataCenterInfo:{
+            "@class": "com.netflix.appinfo.InstanceInfo$DefaultDataCenterInfo",
+            name: "MyOwn"
+        }
+
+    },
+    eureka: {
+        host: "localhost",
+        port: 8761,
+        servicePath: "/eureka/apps",  // Corrected service path
+    },
 
 })
 
 app.use('/Inventory-service',router)
 
 app.listen(port,()=>{
-    console.log('Inventory service running at port : ${port}')
+    console.log(`Inventory service running at port : ${port}`)
+
+    eurekaClient.start((error)=>{
+        if(error){
+            console.log("fail to register eureka")
+        }else {
+            console.log('registered successfully')
+        }
+    })
+
 })
